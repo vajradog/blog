5024712975dc1ec87a35152c13aa81724dcbd456
vajradog <vajradog@gmail.com>
2014-07-17 08:37:51 -0400

    added excerpt column, changed view template accordingly, added visibility to meta infos, including time to read

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,13 @@
 module ApplicationHelper
 
- # def display_datetime(dt)
-  #  dt.strftime("%B %e")
-  #end
+ def display_datetime(dt)
+   dt.strftime("%B %e")
+ end
 
-  # def word_count(post)
-   #  (post.body.to_s.split.size  / 214).round
-  #end
+def word_count(post)
+  (post.body.to_s.split.size  / 214).round
+end
+
 def markdown(text)
     options = {
       filter_html:     false,
